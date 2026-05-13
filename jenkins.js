d0e434accfce5f7dd1c801739f2ee29aa15692d0
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2026-05-13 11:00:43 +0100

    9

--- jenkins.js
+++ jenkins.js
@@ -1,1 +1,16 @@
-test;
+pipeline {
+    agent any
+
+    stages {
+        stage('Deploy') {
+            steps {
+                    git 'https://github.com/simoncobb1966/jenkinstest.git/'
+                    ftpPublisher alwaysPublishFromMaster: true, masterNodeName: '', paramPublish: [parameterName: ""],continueOnError: false, failOnError: false, publishers: [
+        [configName: 'HostPresto', transfers: [
+            [asciiMode: false, cleanRemote: false, excludes: '', flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: "/public_html/jenkins", remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**.exe, **.txt']
+        ], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: true]
+    ]
+            }
+        }
+    }
+}
